9b0a3ee6e6c21a596934bd028ff183dd64bd62bd
iliya-zver <muromiliya786@gmail.com>
2026-04-28 17:34:06 +0500

    work home

--- lesson3.6.py
+++ lesson3.6.py
@@ -3,8 +3,14 @@ cards = int(input("Количество видеокарт: "))
 list_card = []
 
 for i_card in range(1, cards + 1):
-    print("Видеокарта", i_card, ": ")
+    print("Видеокарта", i_card, ": ", end='')
     card = int(input())
     list_card.append(card)
 
-print(list_card)
+old_cards = []
+
+for i in list_card:
+    if i != max(list_card):
+        old_cards.append(i)
+
+print(old_cards)
