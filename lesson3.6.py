099f9a65dbce48a67bb6c3606665fe0ff49ab954
iliya-zver <muromiliya786@gmail.com>
2026-04-29 10:08:33 +0500

    add new films

--- lesson3.6.py
+++ lesson3.6.py
@@ -1,16 +1,37 @@
-cards = int(input("Количество видеокарт: "))
+# cards = int(input("Количество видеокарт: "))
+#
+# list_card = []
+#
+# for i_card in range(1, cards + 1):
+#     print("Видеокарта", i_card, ": ", end='')
+# #     card = int(input())
+# #     list_card.append(card)
+# #
+# # old_cards = []
+# #
+# # for i in list_card:
+# #     if i != max(list_card):
+# #         old_cards.append(i)
+# #
+# # print(old_cards)
 
-list_card = []
+films = ["Крепкий орешек", "Назад в будущее", "Таксист",
+    "Леон", "Богемская рапсодия", "Город грехов",
+    "Мементо", "Отступники", "Деревня"]
 
-for i_card in range(1, cards + 1):
-    print("Видеокарта", i_card, ": ", end='')
-    card = int(input())
-    list_card.append(card)
+name = int(input("Сколько фильмов хотите добавить? "))
 
-old_cards = []
+new_films = []
 
-for i in list_card:
-    if i != max(list_card):
-        old_cards.append(i)
+for i in range(name):
+    print("Введите название фильма: ", end='')
+    film = input()
+    if film in films:
+        new_films.append(film)
+    else:
+        print("Ошибка: фильма", film, "у нас нет.")
 
-print(old_cards)
+print("Ваш список любимых фильмов: ", end=' ')
+for i in new_films:
+    print(i, end=", ")
+
