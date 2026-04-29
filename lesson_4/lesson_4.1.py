3f1b072b4980622300c07760cdc8cf3991fb1acf
iliya-zver <muromiliya786@gmail.com>
2026-04-29 17:35:06 +0500

    add new cod

---
+++ lesson_4/lesson_4.1.py
@@ -0,0 +1,12 @@
+def selection_sort(my_list):
+    for i_mn in range(len(my_list)):
+        for curr in range(i_mn, len(my_list)):
+            if my_list[curr] < my_list[i_mn]:
+                my_list[curr], my_list[i_mn] = my_list[i_mn], my_list[curr]
+
+
+nums = [4, 9, 7, 6, 3, 2]
+
+selection_sort(nums)
+
+print(nums)
