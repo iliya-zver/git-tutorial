893f70a7aaa8c1d4cfad3796dc715f6e7d00d241
iliya-zver <muromiliya786@gmail.com>
2026-05-14 13:38:23 +0500

    add new lesson_4.2

--- lesson_4/lesson_4.2.py
+++ lesson_4/lesson_4.2.py
@@ -1,25 +1,18 @@
 def is_palindrome(num_list):
-    revers_list = []
-    for i_num in range(len(num_list) - 1, -1, -1):
-        revers_list.append(num_list[i_num])
+    revers_list = num_list[::-1]
     if num_list == revers_list:
         return True
     else:
         return  False
 
 nums = [1, 2, 3, 4, 5]
-new_nums =[]
 answer = []
 
 for i_nums in range(0, len(nums)):
-    for j_elem in range(i_nums, len(nums)):
-        new_nums.append(nums[j_elem])
-    if is_palindrome(new_nums):
-        for i_answer in range(0, i_nums):
-            answer.append(nums[i_answer])
+    if is_palindrome(nums[i_nums:len(nums)]):
+        answer = nums[:i_nums]
         answer.reverse()
         break
-    new_nums = []
 
 
 print('Исходный список: ',nums)
