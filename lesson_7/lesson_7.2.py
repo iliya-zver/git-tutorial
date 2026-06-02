feb4ede0f7f6706a3d51d7aa60050b7a9a432c14
iliya-zver <muromiliya786@gmail.com>
2026-06-02 17:23:26 +0500

    add new lesson code

---
+++ lesson_7/lesson_7.2.py
@@ -0,0 +1,27 @@
+# def add_num(seq, number):
+#     seq = list(seq)
+#     for i_num in range(len(seq)):
+#         seq[i_num] += number
+#     return seq
+#
+# origin_tuple = (3, 1, 4, 1, 5)
+# change_list = add_num(origin_tuple, 5)
+#
+# print(origin_tuple)
+# print(change_list)
+
+# import random
+#
+# new_list = [random.randint(0, 5) for i in range(10)]
+# new_list_2 = [random.randint(-5, 0) for i in range(10)]
+#
+#
+# new_tuple = tuple(new_list)
+# new_tuple_2 = tuple(new_list_2)
+# new_tuple_3 = new_tuple + new_tuple_2
+# nul = new_tuple_3.count(0)
+#
+# print(new_tuple_3)
+# print(nul)
+
+
